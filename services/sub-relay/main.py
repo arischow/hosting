e67651fea82b2e81dee4a2fe96a3d0e5813783e3
Aris Chow <hello@arisc.how>
2023-08-27 23:06:24 +0800

    sub-relay

---
+++ services/sub-relay/main.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, request
+
+project_root = os.path.dirname(__file__)
+app = Flask(__name__)
+
+if not os.getenv("AUTH_TOKEN"):
+    raise Exception("AUTH_TOKEN not set")
+
+
+@app.route("/<token>")
+def retrieve(token):
+    try:
+        with open(f"{project_root}/data/{token}", "r") as f:
+            return f.read()
+    except FileNotFoundError:
+        return "No access", 400
+
+
+@app.route("/<token>", methods=["POST"])
+def save(token):
+    if request.headers.get("Authorization") != os.getenv("AUTH_TOKEN"):
+        return "Unauthorized", 401
+    with open(f"{project_root}/data/{token}", "w") as f:
+        f.write(request.data.decode("utf-8"))
+    return "OK", 201
